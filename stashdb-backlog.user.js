fa74e13640bd2dbe54e7223fc90e8268c99a7d8e
peolic <66393006+peolic@users.noreply.github.com>
2021-06-11 20:52:46 +0300

    stop spamming github
    set and save the last-checked timestamp (d'oh!)

--- stashdb-backlog.user.js
+++ stashdb-backlog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      StashDB Backlog
 // @author    peolic
-// @version   1.12.2
+// @version   1.12.3
 // @namespace https://gist.github.com/peolic/e4713081f7ad063cd0e91f2482ac39a7/raw/stashdb-backlog.user.js
 // @updateURL https://gist.github.com/peolic/e4713081f7ad063cd0e91f2482ac39a7/raw/stashdb-backlog.user.js
 // @grant     GM.setValue
@@ -307,10 +307,17 @@ async function inject() {
             `[backlog] data index lastest remote update: ${formatDate(lastUpdated)}`
             + ` - updating: ${shouldFetchIndex}`
           );
+
+          if (!shouldFetchIndex) {
+            // Use this as a "last checked" timestamp as to not spam GitHub API
+            storedDataIndex.lastUpdated = new Date().toISOString();
+            //@ts-expect-error
+            await GM.setValue(DATA_INDEX_KEY, JSON.stringify(storedDataIndex));
+          }
         }
       }
     } catch (error) {
-      console.error('[backlog] error trying to determine lastest data index update');
+      console.error('[backlog] error trying to determine lastest data index update', error);
       shouldFetchIndex = shouldFetch(storedDataIndex, 1);
     }
 
