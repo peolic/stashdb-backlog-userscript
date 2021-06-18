770ceef245d096de1ff5b730dc10114ad98b108b
peolic <66393006+peolic@users.noreply.github.com>
2021-06-18 05:25:17 +0300

    icon

--- stashdb-backlog.user.js
+++ stashdb-backlog.user.js
@@ -2,6 +2,7 @@
 // @name      StashDB Backlog
 // @author    peolic
 // @version   1.18.0
+// @icon      https://cdn.discordapp.com/attachments/559159668912553989/841890253707149352/stash2.png
 // @namespace https://gist.github.com/peolic/e4713081f7ad063cd0e91f2482ac39a7/raw/stashdb-backlog.user.js
 // @updateURL https://gist.github.com/peolic/e4713081f7ad063cd0e91f2482ac39a7/raw/stashdb-backlog.user.js
 // @grant     GM.setValue
