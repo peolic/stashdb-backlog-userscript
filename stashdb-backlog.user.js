a0a15a9d28b70c5a6c1e4f3f24bed2d4858cfe3d
peolic <66393006+peolic@users.noreply.github.com>
2021-07-06 22:18:22 +0300

    refactor `genderIcon`

--- stashdb-backlog.user.js
+++ stashdb-backlog.user.js
@@ -202,7 +202,7 @@ async function inject() {
   // =====
 
   /**
-   * @template {HTMLElement} E
+   * @template {HTMLElement | SVGSVGElement} E
    * @param {E} el
    * @param {Partial<CSSStyleDeclaration>} styles
    * @returns {E}
@@ -903,32 +903,39 @@ async function inject() {
       })));
 
   // SVG is rendered huge if FontAwesome was tree-shaken in compilation?
-  const svgStyleFix = [
-    'overflow: visible', // svg:not(:root).svg-inline--fa
-    'width: 1.125em', // .svg-inline--fa.fa-w-18
-    'display: inline-block', // .svg-inline--fa
-    'font-size: inherit', // .svg-inline--fa
-    'height: 1em', // .svg-inline--fa
-    'overflow: visible', // .svg-inline--fa
-    'vertical-align: -0.125em', // .svg-inline--fa
-  ].join('; ');
+  const svgStyleFix = {
+    overflow: 'visible', // svg:not(:root).svg-inline--fa || .svg-inline--fa
+    width: '1.125em', // .svg-inline--fa.fa-w-18
+    display: 'inline-block', // .svg-inline--fa
+    fontSize: 'inherit', // .svg-inline--fa
+    height: '1em', // .svg-inline--fa
+    verticalAlign: '-0.125em', // .svg-inline--fa
+  };
 
-  const genderIcon = (/** @type {boolean} */ fixStyle) => (
-    '<svg'
-    + (fixStyle ? ` style="${svgStyleFix}"`: '')
-    + ' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="venus-mars" role="img"'
-    + ' class="svg-inline--fa fa-venus-mars fa-w-18 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">'
-      + '<path fill="currentColor" d="M564 0h-79c-10.7 0-16 12.9-8.5 20.5l16.9 16.9-48.7 48.7C422.5 72.1'
-      + ' 396.2 64 368 64c-33.7 0-64.6 11.6-89.2 30.9 14 16.7 25 36 32.1 57.1 14.5-14.8 34.7-24 57.1-24'
-      + ' 44.1 0 80 35.9 80 80s-35.9 80-80 80c-22.3 0-42.6-9.2-57.1-24-7.1 21.1-18 40.4-32.1 57.1 24.5'
-      + ' 19.4 55.5 30.9 89.2 30.9 79.5 0 144-64.5 144-144 0-28.2-8.1-54.5-22.1-76.7l48.7-48.7 16.9 16.9c2.4'
-      + ' 2.4 5.4 3.5 8.4 3.5 6.2 0 12.1-4.8 12.1-12V12c0-6.6-5.4-12-12-12zM144 64C64.5 64 0 128.5 0 208c0'
-      + ' 68.5 47.9 125.9 112 140.4V400H76c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h36v36c0 6.6 5.4 12 12'
-      + ' 12h40c6.6 0 12-5.4 12-12v-36h36c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-36v-51.6c64.1-14.6'
-      + ' 112-71.9 112-140.4 0-79.5-64.5-144-144-144zm0 224c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80'
-      + ' 80-35.9 80-80 80z"></path>'
-    + '</svg>'
-  );
+  /**
+   * @param {boolean} fixStyle
+   * @returns {SVGSVGElement}
+   */
+  const genderIcon = (fixStyle) => {
+    const div = document.createElement('div');
+    div.innerHTML = (
+      '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="venus-mars" role="img"'
+      + ' class="svg-inline--fa fa-venus-mars fa-w-18 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">'
+        + '<path fill="currentColor" d="M564 0h-79c-10.7 0-16 12.9-8.5 20.5l16.9 16.9-48.7 48.7C422.5 72.1'
+        + ' 396.2 64 368 64c-33.7 0-64.6 11.6-89.2 30.9 14 16.7 25 36 32.1 57.1 14.5-14.8 34.7-24 57.1-24'
+        + ' 44.1 0 80 35.9 80 80s-35.9 80-80 80c-22.3 0-42.6-9.2-57.1-24-7.1 21.1-18 40.4-32.1 57.1 24.5'
+        + ' 19.4 55.5 30.9 89.2 30.9 79.5 0 144-64.5 144-144 0-28.2-8.1-54.5-22.1-76.7l48.7-48.7 16.9 16.9c2.4'
+        + ' 2.4 5.4 3.5 8.4 3.5 6.2 0 12.1-4.8 12.1-12V12c0-6.6-5.4-12-12-12zM144 64C64.5 64 0 128.5 0 208c0'
+        + ' 68.5 47.9 125.9 112 140.4V400H76c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h36v36c0 6.6 5.4 12 12'
+        + ' 12h40c6.6 0 12-5.4 12-12v-36h36c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-36v-51.6c64.1-14.6'
+        + ' 112-71.9 112-140.4 0-79.5-64.5-144-144-144zm0 224c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80'
+        + ' 80-35.9 80-80 80z"></path>'
+      + '</svg>'
+    );
+    const svg = div.getElementsByTagName('svg')[0];
+    if (fixStyle) setStyles(svg, svgStyleFix);
+    return svg;
+  };
 
   /**
    * @param {DataObject | null} data
@@ -1303,8 +1310,7 @@ async function inject() {
           pa.href = `/performers/${entry.id}`;
         }
 
-        pa.insertAdjacentHTML('afterbegin', genderIcon(existingPerformers.length === 0));
-        pa.append(...nameElements(entry));
+        pa.append(genderIcon(existingPerformers.length === 0), ...nameElements(entry));
         return pa;
       };
 
