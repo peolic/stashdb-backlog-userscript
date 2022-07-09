4e2d390066f99ecf8dc68ffb00674715ba67ccda
peolic <66393006+peolic@users.noreply.github.com>
2022-07-09 16:16:18 +0300

    hide performer fragments search if not useful

--- stashdb-backlog.user.js
+++ stashdb-backlog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        StashDB Backlog
 // @author      peolic
-// @version     1.31.5
+// @version     1.31.6
 // @description Highlights backlogged changes to scenes, performers and other entities on StashDB.org
 // @icon        https://cdn.discordapp.com/attachments/559159668912553989/841890253707149352/stash2.png
 // @namespace   https://github.com/peolic
@@ -3628,10 +3628,13 @@ button.nav-link.backlog-flash {
         const params = new URLSearchParams();
         if (fragment.id) params.append('id', fragment.id);
         fragment.links?.forEach((link) => params.append('url', link));
-        const fragmentSearch = makeLink(`/pfragments?${params.toString()}`, '🔎');
-        fragmentSearch.classList.add('me-1', 'fw-bold', 'text-decoration-none');
-        fragmentSearch.title = 'Search for other fragments...';
-        fragmentEl.appendChild(fragmentSearch);
+        const fragmentSearchQS = params.toString();
+        if (fragmentSearchQS) {
+          const fragmentSearch = makeLink(`/pfragments?${fragmentSearchQS}`, '🔎');
+          fragmentSearch.classList.add('me-1', 'fw-bold', 'text-decoration-none');
+          fragmentSearch.title = 'Search for other fragments...';
+          fragmentEl.appendChild(fragmentSearch);
+        }
 
         let fragmentName;
         if (fragment.id) {
